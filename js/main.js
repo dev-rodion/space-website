5edc526e780653f44f0aa3e3591b3494d52b9cdd
dev-rodion <gudkov.rodion.21@gmail.com>
2021-11-21 18:05:11 +0300

    Added styles. Added glider.min.js to make a slider.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$(function(){
-
-});
+window.onload = function () {
+  new Glider(document.getElementById("slider"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    rewind: true,
+    draggable: true,
+    dragVelocity: 2,
+    arrows: {
+      prev: ".arrow_prev",
+      next: ".arrow_next",
+    },
+  });
+};
