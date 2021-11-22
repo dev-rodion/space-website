46a178b339295a303688b83aaf9275bfd2ce691f
dev-rodion <gudkov.rodion.21@gmail.com>
2021-11-22 17:28:30 +0300

    Deleted jquery.min.js and grider slider. Added tiny-slider.min.js to make a slider.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 window.onload = function () {
-  new Glider(document.getElementById("slider"), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    scrollLock: true,
-    rewind: true,
-    draggable: true,
-    dragVelocity: 2,
-    arrows: {
-      prev: ".arrow_prev",
-      next: ".arrow_next",
-    },
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+  for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const id = smoothLink.getAttribute("href");
+
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+
+  var slider = tns({
+    container: ".slider",
+    items: 1,
+    slideBy: "page",
+    mouseDrag: true,
+    swipeAngle: false,
+    lazyload: true,
+    speed: 400,
+    controlsContainer: ".slider__arrows",
+    nav: false,
   });
+
+  // new Glider(document.getElementById("slider"), {
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   scrollLock: true,
+  //   rewind: true,
+  //   draggable: true,
+  //   dragVelocity: 2,
+  //   arrows: {
+  //     prev: ".arrow_prev",
+  //     next: ".arrow_next",
+  //   },
+  // });
 };
