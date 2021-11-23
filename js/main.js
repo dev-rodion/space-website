e0e236b72f98d93d4f2007af86269bca19eab794
dev-rodion <gudkov.rodion.21@gmail.com>
2021-11-23 12:11:14 +0300

    Updated index.html, style.css and main.js. Added mobile menu and burger.

--- js/main.js
+++ js/main.js
@@ -16,6 +16,44 @@ window.onload = function () {
     });
   }
 
+  //
+  const burger = document.querySelector(".burger");
+  const navbar = document.querySelector(".navbar");
+  const navbarBg = document.querySelector(".navbar__bg");
+
+  const navLinks = document.querySelectorAll(".navbar__link");
+
+  console.log(navbar.className.includes("active"), navbar.className);
+
+  burger.addEventListener("click", function () {
+    if (navbar.className.includes("active")) {
+      hideNav();
+    } else {
+      showNav();
+    }
+  });
+
+  navbarBg.addEventListener("click", hideNav);
+
+  for (const navLink of navLinks) {
+    navLink.addEventListener("click", function () {
+      hideNav();
+    });
+  }
+
+  function showNav() {
+    document.body.style.overflow = "hidden";
+    burger.classList.add("burger_active");
+    navbar.classList.add("navbar_active");
+    navbarBg.classList.add("navbar__bg_active");
+  }
+  function hideNav() {
+    document.body.style.overflow = "";
+    burger.classList.remove("burger_active");
+    navbar.classList.remove("navbar_active");
+    navbarBg.classList.remove("navbar__bg_active");
+  }
+
   // Tiny Slider
   var slider = tns({
     container: ".slider",
